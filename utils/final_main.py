5298d04912496e4134ba844cbd99236c16fc0bc6
Ceren Morey <cerenmorey@hotmail.com>
2021-07-23 16:26:59 +0200

    Update final_main.py

--- utils/final_main.py
+++ utils/final_main.py
@@ -1,7 +1,9 @@
-from atefeh_apartment import GBR_apartment
-from atefeh_house import GBR_House
-from atefeh import gradientboostingregressor
-
+from utils.a_import_raw_data import preprocess
+from utils.d_model_global_all_features import model_global_all_features
+from utils.e_multiple_linear import multiple_regression_selected_features
+from utils.f_gradient_boosting import gradientboostingregressor
+from utils.g_gradient_boosting_apartment import GBR_apartment
+from utils.h_gradient_boosting_house import GBR_House
 
 
 def linear_regression_models_all_results():
@@ -15,7 +17,7 @@ def linear_regression_models_all_results():
     ds = preprocess()
 
     #call the global multiple linear regression and report its score
-    global_model_result = model_global_all features(ds)
+    global_model_result = model_global_all_features(ds)
     print(global_model_result)
 
     #call the model of multiple linear regression with selected features and report its score
@@ -35,11 +37,3 @@ def linear_regression_models_all_results():
     print(gradient_boosting_regressor_for_apartment_result)
 
 linear_regression_models_all_results()
-
-
-
-
-
-
-
-
